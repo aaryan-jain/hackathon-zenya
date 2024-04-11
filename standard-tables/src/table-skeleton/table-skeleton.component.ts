2e5b71803d764f3c06bbdfe6e929d8b2b78d5b58
Aaryan Jain <34703700+aaryan-jain@users.noreply.github.com>
2024-04-11 10:07:31 +0530

    Initial commit

---
+++ standard-tables/src/table-skeleton/table-skeleton.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table-skeleton',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table-skeleton.component.html',
+  styleUrls: ['./table-skeleton.component.css']
+})
+export class TableSkeletonComponent {
+
+}
